727d16e68d48fc0818f666696d3e7a666b1485d9
pranav1034 <pranavaggarwal04@gmail.com>
2025-05-07 13:22:37 +0530

    [Pranav] ADD: hashset java problem

--- HashsetStrings.java
+++ HashsetStrings.java
@@ -1,14 +1,33 @@
-import java.util.HashSet;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class HashsetStrings {
-    HashSet<String> set=new HashSet<>();
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+        HashSet<String> set=new HashSet<>();
         for(int i=0;i<t;i++){
-        String s1=pair_left[i]+" "+pair_right[i];
-        String s2=pair_right[i]+" "+pair_left[i];
+            String s1=pair_left[i]+" "+pair_right[i];
+            String s2=pair_right[i]+" "+pair_left[i];
 
-        if(!set.contains(s2))
-            set.add(s1);
+            if(!set.contains(s2))
+                set.add(s1);
 
-        System.out.println(set.size());
+            System.out.println(set.size());
+        }
+
     }
 }
